6d7a254deb4f21ef894cdb76ef7f97765272320e
Vinicius Mamoré <vinicius.mamore@gmail.com>
2021-08-23 23:05:20 -0400

    removing UserContext reference

--- desafio-reactjs/src/App.js
+++ desafio-reactjs/src/App.js
@@ -1,18 +1,14 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import { UserContextProvider } from './context/UserContext';
-
 import { Home } from './pages/Home';
 import { Profile } from './pages/Profile';
 
 function App() {
   return (
     <BrowserRouter>
-      <UserContextProvider>
         <Route path="/" exact component={Home} />
         <Route path="/home" exact component={Home} />
         <Route path="/profile/:username" exact component={Profile} />
-      </UserContextProvider>
     </BrowserRouter>
   );
 }
