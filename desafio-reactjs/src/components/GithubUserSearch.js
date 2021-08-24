de95e21c57bfd5e476d7f85a1aa38735d2a2eab4
Vinicius Mamoré <vinicius.mamore@gmail.com>
2021-08-23 23:04:08 -0400

    refactoring to pass username as a param insted of making request

--- desafio-reactjs/src/components/GithubUserSearch.js
+++ desafio-reactjs/src/components/GithubUserSearch.js
@@ -1,98 +1,52 @@
-import { useState } from "react"
+import { useState } from "react";
 
-import { useHistory } from 'react-router-dom';
-import { Button, TextField } from '@material-ui/core';
+import { useHistory } from "react-router-dom";
+import { Button, TextField } from "@material-ui/core";
 
-import axios from 'axios';
+import SearchIcon from "@material-ui/icons/Search";
 
-import SearchIcon from '@material-ui/icons/Search';
-import CachedIcon from '@material-ui/icons/Cached';
+import { verifyUserExist } from "../services/github-api";
 
-import { useUser } from "../hooks/useUser";
-
-import '../styles/github-user-search.scss';
+import "../styles/github-user-search.scss";
 
 export function GithubUserSearch() {
-    const history = useHistory();
+  const history = useHistory();
 
-    const [username, setUsername] = useState('');
-    const [isLoading, setLoading] = useState(false);
+  const [username, setUsername] = useState("");
 
-    const { setUser } = useUser();
-    
+  async function fetchUser(event) {
+    event.preventDefault();
 
-    const url = `https://api.github.com/users/${username}`;
-
-    function fetchGithubProfileData(event) {
-        event.preventDefault();
-
-        if(username.trim() === '') {
-            alert('Username cannot be empty.')
-            return;
-        }
-
-        setLoading(true);
-
-        axios.get(url).then(userResponse => {
-                var reposEndpoint = userResponse.data.repos_url;
-                axios.get(reposEndpoint)
-                    .then(repoResponse => {
-                        setUser({
-                            name: userResponse.data.name,
-                            bio: userResponse.data.bio,
-                            photo: userResponse.data.avatar_url,
-                            username: userResponse.data.login,
-                            location: userResponse.data.location,
-                            followers: userResponse.data.followers,
-                            following: userResponse.data.following,
-                            organization: userResponse.data.company,
-                            email: userResponse.data.email,
-                            blog: userResponse.data.blog,
-                            twitter: userResponse.data.twitter_username,
-                            repos: repoResponse.data.map( repo => { 
-                                return {
-                                    url: repo.html_url,
-                                    lastUpdate: repo.updated_at,
-                                    stars: repo.stargazers_count,
-                                    name: repo.name,
-                                    description: repo.description
-                                } 
-                            })
-                        });
-                        setLoading(false);
-                        history.push('/profile');
-                    })
-            })
-            .catch(() => {
-                alert('Error when trying to fetch user.');
-                setLoading(false);
-                setUsername('');
-            });  
+    if (username.trim() === "") {
+      alert("Username cannot be empty.");
+      return;
     }
 
-    return (
-        <div id="user-search-page">
-            <form onSubmit={fetchGithubProfileData}>
-                <TextField
-                    placeholder="Type the username here..."
-                    variant="outlined"
-                    type="text"
-                    size="small"
-                    value={username}
-                    disabled={isLoading}
-                    onChange={e => setUsername(e.target.value)}
-                    >
-                </TextField>
-                { !isLoading ? 
-                (
-                <Button variant="contained" color="primary" type="submit">
-                <SearchIcon /> Buscar
-                </Button>)
-                : (
-                <Button variant="contained" color="primary" type="submit" disabled>
-                <CachedIcon />
-                </Button>)}
-            </form>
-        </div>
-    )
+    const userExist = await verifyUserExist(username);
+
+    if (!userExist) {
+      alert("Invalid username. Please insert correct username.");
+      return;
+    }
+
+    history.push(`/profile/${username}`);
+  }
+
+  return (
+    <div id="user-search-page">
+      <form onSubmit={fetchUser}>
+        <TextField
+          placeholder="Type the username here..."
+          variant="outlined"
+          type="text"
+          size="small"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        ></TextField>
+        <Button variant="contained" color="primary" type="submit">
+          <SearchIcon /> Buscar
+        </Button>
+      </form>
+    </div>
+  );
 }
