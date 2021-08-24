0420fc3d86c9d01f65889dd6b0b8d43d6ad1ba97
Vinicius Mamoré <vinicius.mamore@gmail.com>
2021-08-23 23:05:34 -0400

    Refactor to use useEffects

--- desafio-reactjs/src/pages/Profile.js
+++ desafio-reactjs/src/pages/Profile.js
@@ -1,32 +1,52 @@
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 
-import { useUser } from "../hooks/useUser";
 import { Button } from "@material-ui/core";
 
 import { RepositoryList } from "../components/RepositoryList";
 import { UserInfo } from "../components/UserInfo";
+import { fetchGithubProfileData } from "../services/github-api";
 
-import '../styles/profile.scss';
+import "../styles/profile.scss";
 
-export function Profile() {
-    const { user } = useUser();
-    const history = useHistory();
-    
-    function returnToHome() {
-        history.push('/');
-    }
+export function Profile(props) {
+  const [user, setUser] = useState({});
+  const history = useHistory();
 
-    return (
-        <div id="profile-page">
-            <aside>
-                <UserInfo user={user} />
-                <Button variant="contained" color="default" size="large" onClick={returnToHome}>Voltar</Button>
-            </aside>
-            <main>
-                <div className="main-content">
-                    { user && user.repos && <RepositoryList repositories={user.repos} /> }
-                </div>
-            </main>  
-        </div>
-    )
+  useEffect(() => {
+    fetchGithubProfileData(props.match.params.username).then((user) => {
+      setUser(user);
+    });
+  }, [props.match.params.username]);
+
+  function returnToHome() {
+    history.push("/");
+  }
+
+  return (
+    <div id="profile-page">
+      {user ? (
+        <>
+          <aside>
+            <UserInfo user={user} />
+            <Button
+              variant="contained"
+              color="default"
+              size="large"
+              onClick={returnToHome}
+            >
+              Voltar
+            </Button>
+          </aside>
+          <main>
+            <div className="main-content">
+              {user && user.repos && (
+                <RepositoryList repositories={user.repos} />
+              )}
+            </div>
+          </main>
+        </>
+      ) : null}
+    </div>
+  );
 }
